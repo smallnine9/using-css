70b0dd35b83c96ad1c463ae29bb32aee32e12e80
yulj5 <yulj5@asiainfo.com>
2022-11-04 17:09:17 +0800

    chore:todo EVA font：出击

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
-    <Footer />
+    <div text="center" style="width: 420px;height: 60px;background-color: red;display: inline-block;overflow: hidden;">
+      <div style="margin-bottom: 55px;position:relative;animation:mymove 8s infinite;height: 10px;width:300px;background-color:black;transform: rotate(-60deg);" />
+      <div style="margin-bottom: 55px;position:relative;animation:mymove 8s infinite;height: 10px;width:300px;background-color:black;transform: rotate(-60deg);" />
+      <div style="margin-bottom: 55px;position:relative;animation:mymove 8s infinite;height: 10px;width:300px;background-color:black;transform: rotate(-60deg);" />
+      <div style="margin-bottom: 55px;position:relative;animation:mymove 8s infinite;height: 10px;width:300px;background-color:black;transform: rotate(-60deg);" />
+    </div>
+    <div font-extrabold style="font-size: 220px;color:red;animation: 1s blink 3 steps(1);">
+      出擊
+    </div>
+    <div text="center" style="width: 420px;height: 60px;background-color: red;display: inline-block;" />
   </main>
 </template>
+
+<style>
+  @keyframes mymove
+  {
+    0% {right:-200px;}
+    100% {right:200px;}
+  }
+
+  @keyframes blink {
+  50% { color: transparent; }
+  }
+</style>
